36c4c6b7aa4adcb13d36f5a95f00370a3fd4de2f
Rishav <130345151+rishuget@users.noreply.github.com>
2023-07-07 09:55:38 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://techtalkingtop.comg/' + encodeURI(path as string)
+					'https://techtalkingtop.com/' + encodeURI(path as string)
 				}`,
 			},
 		};
